7f11511e1bfdf57fb3d4e44df8ba1e5b17f83ba3
Daniel Oh <dannyoh0316@gmail.com>
2022-06-21 00:24:53 -0500

    return to minimalist polybar

--- .config/polybar/scripts/bitcoin.py
+++ .config/polybar/scripts/bitcoin.py
@@ -6,7 +6,7 @@ API_VERSION = 3
 API_ENDPOINT = f"https://api.coingecko.com/api/v{API_VERSION}/coins/bitcoin"
 
 ICON = "~/.local/share/icons/bitcoin.png"
-NOTIFY_PRICE = 40000
+NOTIFY_PRICE = 18000
 
 try:
     data = get(API_ENDPOINT).json()["market_data"]
