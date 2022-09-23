6f1f2c55dffe54eba0a0301315fc0d31b75e6899
Daniel Oh <danieloh0714@gmail.com>
2022-09-22 19:23:31 -0500

    remove polybar bitcoin notification

--- .config/polybar/scripts/bitcoin.py
+++ .config/polybar/scripts/bitcoin.py
@@ -5,17 +5,9 @@ from subprocess import run
 API_VERSION = 3
 API_ENDPOINT = f"https://api.coingecko.com/api/v{API_VERSION}/coins/bitcoin"
 
-ICON = "~/.local/share/icons/bitcoin.png"
-NOTIFY_PRICE = 18000
-
 try:
     data = get(API_ENDPOINT).json()["market_data"]
     price = data["current_price"]["usd"]
-    if price <= NOTIFY_PRICE:
-        run(
-            f"notify-send -u critical -i {ICON} 'Bitcoin' 'price less than ${NOTIFY_PRICE}'",
-            shell=True,
-        )
     print(f"${price}")
 except ConnectionError as e:
     print("n/a")
